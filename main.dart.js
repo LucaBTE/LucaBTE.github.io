7b49236b292a92c62d6d317c5a8afcfbe038ce18
lucaa <luca.abate03@gmail.com>
2025-05-11 16:11:06 +0200

    trying to fix mobile deployment

--- main.dart.js
+++ main.dart.js
@@ -30608,7 +30608,7 @@ A.aDh.prototype={
 $1(a){var s=A.eX().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/cf56914b326edb0ccb123ffdc60f00060bd513fa/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/18b71d647a292a980abb405ac7d16fe1f0b20434/":s)+a},
 $S:41}
 A.a9p.prototype={
 cQ(a){B.c.am(this.a.a.save())},
